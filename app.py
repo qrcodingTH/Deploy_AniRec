2826d64cbccdc9a4724a87fcfad330422db1adcb
qrcodingTH <142414640+qrcodingTH@users.noreply.github.com>
2024-06-11 14:50:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -122,13 +122,14 @@ with open(label_map_path, 'w') as f:
 class MyResNeXt101(nn.Module):
     def __init__(self, num_classes=100):
         super(MyResNeXt101, self).__init__()
-        self.network = models.resnext101_32x8d(weights=models.resnext101_32x8d(pretrained=False).weights, pretrained=False)
+        self.network = models.resnext101_32x8d(pretrained=False)
 
         in_features = self.network.fc.in_features
         self.network.fc = nn.Linear(in_features, num_classes)
 
     def forward(self, x):
         return self.network(x)
+
 
 
 # Load the model
